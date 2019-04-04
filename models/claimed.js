69ad9e227f045152d11e8d76188df8276001121a
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-04-04 10:20:57 -0400

    adds classes for all models

--- models/claimed.js
+++ models/claimed.js
@@ -6,6 +6,7 @@ const db = require('./conn');
 class Claimed {
     constructor(id, score, inventory_id, user_id) {
         this.id= id,
+        this.score= score,
         this.inventoryId= inventory_id,
         this.user_id= user_id
     }
