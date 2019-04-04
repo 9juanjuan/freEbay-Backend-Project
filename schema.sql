f03f7bf99e258ca8b2eede8488ba3ff70b6cf239
Rebecca Uranga <urangarebecca@gmail.com>
2019-04-04 10:15:52 -0400

    creates freEbay database

---
+++ schema.sql
@@ -0,0 +1,27 @@
+create table users (
+    id serial primary key,
+    first_name varchar(100), 
+    last_name varchar(100),
+    email varchar(200),
+    password varchar(500)
+);
+
+create table inventory (
+    id serial primary key,
+    item_name varchar(200),
+    category varchar(200),
+    price varchar(200),
+    content text
+);
+
+create table cart (
+    id serial primary key,
+    user_id integer references users(id),
+    inventory_id integer references inventory(id),
+);
+
+create table claimed (
+    id serial primary key,
+    user_id integer references users(id),
+    inventory_id integer references inventory(id),
+);
---
+++ seed.sql
@@ -0,0 +1,35 @@
+insert into users
+    (first_name, last_name, email, password)
+values
+    ('Rebecca', 'Uranga', 'rebeccaiscool@gmail.com', 'ilovemycat'),
+    ('Antonio', 'Garcia', 'antonioiskindacool@gmail.com', 'ilovecodingsomuch'),
+    ('Sebby', 'Kitty', 'yumyumtuna@gmail.com', 'tuna4life'),
+    ('Momo', 'Katz', 'popcornlover@gmail.com', 'snacksandnaps')
+;
+
+insert into inventory 
+    (item_name, category, price, content);
+values 
+    ('Beats Headphones', 'Electronics', '$299', 'Like new!'),
+    ('Cat Food', 'Pet Supplies', '$10', 'Organic, glutenfree.'),
+    ('Coding for Dummies', 'Books', '$15', 'Lightly used'),
+    ('Phone Case', 'Electronics', '$20', 'Great deal!'),
+    ('Cat Litter', 'Pet Supplies', '$12', 'Covers the smell of poop!'),
+    ('Backend Development for Dummies', 'Books', '$25', 'You will learn soooo much!')
+;
+
+insert into cart 
+    (user_id, inventory_id);
+values 
+    (2, 1),
+    (3, 2),
+    (2, 3),
+    (1, 6),
+    (1, 4),
+    (4, 2),
+;
+
+insert into claimed
+    (user_id, inventory_id);
+values
+    ();
